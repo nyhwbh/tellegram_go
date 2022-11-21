4b1429fee28deea59767ffd786787c8f5186e1b8
nyhwbh <87648321+nyhwbh@users.noreply.github.com>
2022-11-21 21:39:00 +0900

    add WorkComponent

---
+++ front-end/src/components/TG-WorkComponent.vue
@@ -0,0 +1,28 @@
+<template>
+    <div class="work_board">
+        <todoApp></todoApp>
+        <div clclass="eoullim_calendar"></div>
+    </div>
+</template>
+
+<script>
+import todoApp from './co-work/TG-TodoApp.vue'
+export default {
+    components:{
+        todoApp,
+    }
+}
+</script>
+
+<style>
+.work_board{
+    background-color: bisque;
+    position: absolute;
+    padding: 30px;
+    top: 7%;
+    left: 3%;
+    height: 85%;
+    width: 91%;
+    border-radius: 30px;
+}
+</style>
